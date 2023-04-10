0fa0fa78c5e326daa98663a9bc1494cd2ed48abd
Hasan Ali Rawjee <hasanalirawjee@hasans-mbp.mynetworksettings.com>
2023-04-09 23:27:42 -0400

    a9 mongo sp23

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -1,4 +1,3 @@
-// in a9, retrieve data from mongodb
 import * as tuitsDao from '../tuits/tuits-dao.js'
 
 const currentUser = {
@@ -47,41 +46,6 @@ const updateTuit = async (req, res) => {
     const status = await tuitsDao.updateTuit(tuitIdToUpdate, updates);
     res.json(status);
 }
-
-// in a8, we retrieve tuits data from self-defined js file
-// import posts from "./tuits.js";
-//
-// let tuits = posts;
-//
-// const createTuit = (req, res) => {
-//     let newTuit = req.body;
-//     newTuit._id = (new Date()).getTime();
-//     newTuit = {
-//         ...templateTuit,
-//         ...newTuit
-//     }
-//     tuits.unshift(newTuit);
-//     res.json(newTuit);
-// }
-//
-// const findTuits  = (req, res) => res.json(tuits);
-//
-// const updateTuit = (req, res) => {
-//     const tuitIdToUpdate = req.params.tid;
-//     const updates = req.body;
-//     const tuitsIndex = tuits.findIndex(tuit => tuit._id === parseInt(tuitIdToUpdate));
-//     tuits[tuitsIndex] = {
-//         ...tuits[tuitsIndex],
-//         ...updates
-//     };
-//     res.sendStatus(200);
-// }
-//
-// const deleteTuit = (req, res) => {
-//     const tuitIdToDelete = req.params['tid'];
-//     tuits = tuits.filter(tuit => tuit._id !== parseInt(tuitIdToDelete));
-//     res.sendStatus(200);
-// }
 
 export default (app) => {
     app.post('/api/tuits', createTuit);
